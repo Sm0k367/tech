f88ede5e024f40eb991f54d25b4b2c0f135fd6e4
Sm0k367 <139561920+Sm0k367@users.noreply.github.com>
2026-05-25 23:20:41 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { cloudflare } from '@cloudflare/vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    cloudflare()   // Improves Wrangler + Pages integration
-  ],
+  plugins: [react()],
   build: {
     outDir: 'dist',
     rollupOptions: {
